6ce3677d0e35d606d9d6035bedadf728f84572c6
unknown <you@example.com>
2019-12-06 11:49:38 +0800

    Added provider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider} from 'react-redux'
+
+// For testing the store and action
+import store from './store/index'
+import { addCustomer} from './actions/index'
+
+// Just for testing in the console
+// window.store = store;
+// window.addCustomer = addCustomer
+
+ReactDOM.render(
+<Provider>
+    <App />
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import {ADD_CUSTOMER} from '../constants/index'
 
 const initialState = {
-    customers:[]
+    customers:[],
+    current_logged_in_user:0
 }
 
 // all reducers are functions
@@ -14,7 +15,13 @@ export default function rootReducer(state=initialState, action)
 {
     // cannot change/access global variables
     // cannot modify any variables (arrays, objects) directly
-    if (action.type == ADD_CUSTOMER)
+    if (action.type === ADD_CUSTOMER) {
+        const cloned = {
+            ...state,
+            customers: [ ...state.customers, action.payload]
+        }
+        return cloned;
+    }
  
     return state;
 }
