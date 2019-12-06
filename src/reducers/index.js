b1d497f4522f5d6492347aa0f851a9100c9eadfd
unknown <you@example.com>
2019-12-06 11:10:14 +0800

    Added a store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,7 +8,7 @@ const initialState = {
 // 3. and an action
 // if we can rootReducer() without any argument,
 // then the state argument will be that of the initialState
-function rootReducer(state=initialState, action)
+export default function rootReducer(state=initialState, action)
 {
     // cannot change/access global variables
     // cannot modify any variables (arrays, objects) directly
