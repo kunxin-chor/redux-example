13fc4f4e96c2bfc40cdeb69346ec26bed5ae70e6
unknown <you@example.com>
2019-12-06 11:08:24 +0800

    Created store and the rootReducer

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const initialState = {
+    customers:[]
+}
+
+// all reducers are functions
+// 1. PURE functions
+// 2. that takes in a state
+// 3. and an action
+// if we can rootReducer() without any argument,
+// then the state argument will be that of the initialState
+function rootReducer(state=initialState, action)
+{
+    // cannot change/access global variables
+    // cannot modify any variables (arrays, objects) directly
+ 
+}
